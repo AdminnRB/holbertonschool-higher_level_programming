462be6953e9ff25132ac3953de83af8e94980cdf
AdminnRB <rsidbagirov62@gmail.com>
2026-06-09 15:56:55 +0000

    Fix pycodestyle E501 line too long in docstring for task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position without modifying the original."""
-    # Hər bir halda ilkin siyahının kopyasını yaradırıq
+    """
+    Replaces an element in a list at a specific position
+    without modifying the original list.
+    """
     list_copy = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return list_copy
